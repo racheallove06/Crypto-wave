145edf58b5ad883e3b44086058ed2ca07e806264
rachealmuchiri <rachealloveo6@gmail.com>
2023-10-14 15:09:31 +0300

    added some files

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import axios from "axios";
+
 const Featured = () => {
   const [data, setData] = useState(null);
   const url =
@@ -23,35 +24,118 @@ const Featured = () => {
   return (
     <div className="featured">
       <div className="container">
-        {/*left*/}
+        {/* Left Section */}
         <div className="left">
-          {" "}
-          <h2>Explore bitcoin and curreny</h2>
+          <h2>Explore bitcoin and currency</h2>
           <p>See all currencies</p>
-          <button className="btn">leran more</button>
+          <button className="btn">Learn more</button>
         </div>
-        {/*right*/}
-        <div className="right">
-          <div className="top">
-            <img src={data[0].image} alt="" />
-          </div>
-          <div>
-            <h2>{data[0].name}</h2>
-            <p>${data[0].current_price.toLocaleString()}</p>
-          </div>
-          {data[0].price_change_percentage_24h < 0 ? (
-            <span className="red"></span>
-          ) : (
-            <span className="green">
-              <AiOutlineArrowDown /> {data[0].price_change_percentage_24h}%{" "}
-            </span>
-          )}
 
-          <span>
-            {" "}
-            <AiOutlineArrowDown />{" "}
-          </span>
+        {/* Right Section - First Card */}
+        <div className="right">
+          {/* Card 1 */}
+          <div className="card">
+            <div className="top">
+              <img src={data[0].image} alt="" />
+            </div>
+            <div>
+              <h2>{data[0].name}</h2>
+              <p>${data[0].current_price.toLocaleString()}</p>
+            </div>
+            {data[0].price_change_percentage_24h < 0 ? (
+              <span className="red">
+                {data[0].price_change_percentage_24h.toFixed(3)}%
+              </span>
+            ) : (
+              <span className="green">
+                <AiOutlineArrowDown />{" "}
+                {data[0].price_change_percentage_24h.toFixed(3)}%{" "}
+              </span>
+            )}
+          </div>
+
+          <div className="card">
+            <div className="top">
+              <img src={data[1].image} alt="" />
+            </div>
+            <div>
+              <h2>{data[2].name}</h2>
+              <p>${data[2].current_price.toLocaleString()}</p>
+            </div>
+            {data[2].price_change_percentage_24h < 0 ? (
+              <span className="red">
+                {data[2].price_change_percentage_24h.toFixed(3)}%
+              </span>
+            ) : (
+              <span className="green">
+                <AiOutlineArrowDown />{" "}
+                {data[2].price_change_percentage_24h.toFixed(3)}%{" "}
+              </span>
+            )}
+          </div>
+
+          <div className="card">
+            <div className="top">
+              <img src={data[3].image} alt="" />
+            </div>
+            <div>
+              <h2>{data[3].name}</h2>
+              <p>${data[3].current_price.toLocaleString()}</p>
+            </div>
+            {data[3].price_change_percentage_24h < 0 ? (
+              <span className="red">
+                {data[3].price_change_percentage_24h.toFixed(3)}%
+              </span>
+            ) : (
+              <span className="green">
+                <AiOutlineArrowDown />{" "}
+                {data[3].price_change_percentage_24h.toFixed(3)}%{" "}
+              </span>
+            )}
+          </div>
+
+          <div className="card">
+            <div className="top">
+              <img src={data[4].image} alt="" />
+            </div>
+            <div>
+              <h2>{data[4].name}</h2>
+              <p>${data[4].current_price.toLocaleString()}</p>
+            </div>
+            {data[4].price_change_percentage_24h < 0 ? (
+              <span className="red">
+                {data[4].price_change_percentage_24h.toFixed(3)}%
+              </span>
+            ) : (
+              <span className="green">
+                <AiOutlineArrowDown />{" "}
+                {data[4].price_change_percentage_24h.toFixed(3)}%{" "}
+              </span>
+            )}
+          </div>
+
+          <div className="card">
+            <div className="top">
+              <img src={data[5].image} alt="" />
+            </div>
+            <div>
+              <h2>{data[5].name}</h2>
+              <p>${data[5].current_price.toLocaleString()}</p>
+            </div>
+            {data[5].price_change_percentage_24h < 0 ? (
+              <span className="red">
+                {data[5].price_change_percentage_24h.toFixed(3)}%
+              </span>
+            ) : (
+              <span className="green">
+                <AiOutlineArrowDown />{" "}
+                {data[5].price_change_percentage_24h.toFixed(3)}%{" "}
+              </span>
+            )}
+          </div>
         </div>
+
+        {/* Add more card sections here with similar structure */}
       </div>
     </div>
   );
